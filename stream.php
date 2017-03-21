6201d9683d3704287bb8646d3a03e8a5dcac51e0
Karel Tuček <kareltucek@centrum.cz>
2017-03-21 13:52:35 +0100

    set higher history limit

--- stream.php
+++ stream.php
@@ -3,7 +3,7 @@ include 'init.php' ;
 include 'cfg.php' ;
 $filename = "'".$TMP_PATH."chatroom".$room."'";
 flush();
-$handle = popen("touch ".$filename." && tail -f ".$filename." 2>&1", 'r');
+$handle = popen("touch ".$filename." && tail -f -n 10000 ".$filename." 2>&1", 'r');
 while(!feof($handle)) {
   $buffer = fgets($handle);
   echo "$buffer\n";
